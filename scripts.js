44659dc1c92515af7425d6bb112909bad90b929b
khaley1125 <156870315+khaley1125@users.noreply.github.com>
2025-02-27 14:01:01 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -80,11 +80,5 @@ const nav = document.querySelector('nav ul');
 menuToggle.addEventListener('click', () => {
     nav.classList.toggle('active');
     menuToggle.classList.toggle('active');
-});// Hamburger Menu Toggle
-const menuToggle = document.querySelector('.menu-toggle');
-const nav = document.querySelector('nav ul');
-
-menuToggle.addEventListener('click', () => {
-    nav.classList.toggle('active');
-    menuToggle.classList.toggle('active');
+});
 });
